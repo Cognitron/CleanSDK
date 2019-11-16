aca1504e6276e406981a9c1e268c9e3322e4d106
Artem Eremeev <jeremy1002@yandex.ru>
2019-11-16 03:04:35 +0300

    Revert "Исправлен Navigator.push для UITabBarController"
    This reverts commit c68c27df4dfa1b7087bc10b6e7a1b7c784b101db.

--- CleanSDK/Classes/Arch/MVVM/Base/Navigator.swift
+++ CleanSDK/Classes/Arch/MVVM/Base/Navigator.swift
@@ -70,14 +70,7 @@ public class Navigator {
     
     public func push<VM: ViewModelType, V: ViewController<VM>>(_ controller: V.Type, viewModel: VM) {
         let controller: V = createController(V.self, viewModel: viewModel)
-        let visibleController = self.visibleController
-        if let tabBarController = visibleController as? UITabBarController {
-            if let navigationController = tabBarController.selectedViewController as? UINavigationController {
-                navigationController.pushViewController(controller, animated: true)
-            }
-        } else {
-            visibleController?.navigationController?.pushViewController(controller, animated: true)
-        }
+        visibleController?.navigationController?.pushViewController(controller, animated: true)
     }
      
     public func pop() {
